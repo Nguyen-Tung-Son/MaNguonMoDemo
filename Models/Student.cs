8379c69cd144cc0ad57b471b783b862f73a6d9e7
1821050999@student.humg.edu.vn <1821050999@student.humg.edu.vn>
2021-11-23 21:15:11 +0700

    Create class Student

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,17 +1,7 @@
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-namespace dotnet.Models
+namespace DemoMVC.Models
 {
-    [Table("Student")]
-    public class Student : Person
+    public class Student 
     {
-        public string Universisty { get; set; }
-        public string Address { get; set; }
-        public string PhoneNumber { get; set; }
-
-
-
-
+        public string StudentID { get; set;}
     }
 }
